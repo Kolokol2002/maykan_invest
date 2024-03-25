5e3657527b961df2c5def3facd83ed0885c52008
Maksym Karalash <Maks.karalash@gmail.com>
2024-03-25 13:57:09 +0200

    Update BackgroungHero.jsx

--- src/common/BackgroungHero/BackgroungHero.jsx
+++ src/common/BackgroungHero/BackgroungHero.jsx
@@ -2,15 +2,26 @@ import { useEffect, useRef } from "react";
 import { BackgroungHeroContainer, VideoHero } from "./BackgroungHero.styled";
 
 const BackgroungHero = ({ children }) => {
-  const videoRef = useRef();
-  useEffect(() => {
-    videoRef.current.playbackRate = 1;
-  }, []);
+  // const videoRef = useRef();
+  // useEffect(() => {
+  //   videoRef.current.playbackRate = 1;
+  // }, []);
 
   return (
     <BackgroungHeroContainer>
-      <VideoHero ref={videoRef} preload="auto" playsinline autoPlay loop muted>
-        <source src="/maykan_invest/for_hero.mp4" type="video/mp4"></source>
+      <VideoHero
+        playsinline
+        autoPlay
+        loop
+        muted
+        data-wf-ignore
+        data-object-fit="cover"
+      >
+        <source
+          src="/maykan_invest/for_hero.mp4"
+          type="video/mp4"
+          data-wf-ignore
+        ></source>
       </VideoHero>
       {children}
     </BackgroungHeroContainer>
